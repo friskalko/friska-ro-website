051c9ebfb120875c91239e44f0da62ba9f690211
Rajat Verma <rajat311201@gmail.com>
2023-01-13 22:49:17 +0530

    created product model

--- server.js
+++ server.js
@@ -5,20 +5,19 @@ dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
 );
 
+mongoose.set('strictQuery', true);
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log('DB connection successful!'));
+    .connect(DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+    console.log(`App running on port ${port}...`);
 });
