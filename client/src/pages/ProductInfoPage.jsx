7825bf74ca194cdc81626720322972e327c53490
Rajat Verma <rajat311201@gmail.com>
2023-02-23 19:52:37 +0530

    added summary and responsive

--- client/src/pages/ProductInfoPage.jsx
+++ client/src/pages/ProductInfoPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { useLoaderData, useNavigation } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 import styles from './ProductInfoPage.module.scss';
-import { ThreeCircles } from 'react-loader-spinner';
+
 import API from '../utils/api';
 
 import ImageSlider from '../components/ImageSlider';
@@ -15,6 +15,7 @@ const ProductInfoPage = () => {
     images.push(
         'https://images.unsplash.com/photo-1676963472951-04a8c0f1f3fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60'
     );
+
     return (
         <div className={styles['container']}>
             {/* <ul>
@@ -28,12 +29,15 @@ const ProductInfoPage = () => {
                 </div>
                 <div className={styles['main-container__info']}>
                     <div>{product.name}</div>
+                    <div className={styles['summary']}> {summ}</div>
                 </div>
             </div>
         </div>
     );
 };
 
+const summ =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vitae neque odio. Curabitur vel pharetra quam. Phasellus non dui elit. Maecenas tempor odio eget mauris rhoncus, in euismod enim pharetra.';
 export default ProductInfoPage;
 
 export const productLoader = async ({ request }) => {
