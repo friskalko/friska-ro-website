da75a4ab97ef21806d6bdcdd7623fb294acf75db
Rajat Verma <rajat311201@gmail.com>
2023-03-19 18:31:28 +0530

    added send enquiry button and modal

--- server/client/src/pages/ProductInfoPage.jsx
+++ server/client/src/pages/ProductInfoPage.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import styles from './ProductInfoPage.module.scss';
 import Specifications from '../components/Specifications';
 import API from '../utils/api';
-
+import { Modal, Box } from '@mui/material';
 import ImageSlider from '../components/ImageSlider';
 const ProductInfoPage = () => {
+    const nameRef = useRef(null);
+    const phoneRef = useRef(null);
+    const emailRef = useRef(null);
+    const countryRef = useRef(null);
+    const descriptionRef = useRef(null);
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     const product = useLoaderData();
 
     const images = [product.imageCover];
@@ -16,33 +25,123 @@ const ProductInfoPage = () => {
         'https://images.unsplash.com/photo-1676963472951-04a8c0f1f3fc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60'
     );
 
+    const formSubmitHandler = (e) => {
+        e.preventDefault();
+        console.log('form submitted');
+        console.log(nameRef, emailRef, phoneRef, descriptionRef);
+    };
+
     return (
-        <div className={styles['container']}>
-            <div className={styles['main-container']}>
-                <div className={styles['main-container__img']}>
-                    <ImageSlider images={images} />
+        <>
+            <div className={styles['container']}>
+                <div className={styles['main-container']}>
+                    <div className={styles['main-container__img']}>
+                        <ImageSlider images={images} />
+                    </div>
+                    <div className={styles['main-container__info']}>
+                        <div>{product.name}</div>
+                        <div className={styles['summary']}> {summ}</div>
+                        <button
+                            className={styles['send-enquiry']}
+                            onClick={handleOpen}
+                        >
+                            Send Enquiry
+                        </button>
+                    </div>
                 </div>
-                <div className={styles['main-container__info']}>
-                    <div>{product.name}</div>
-                    <div className={styles['summary']}> {summ}</div>
+                <div className={styles['description']}>
+                    <h2 className={styles['description__heading']}>
+                        Product Description
+                    </h2>
+                    <p className={styles['description__text']}>
+                        {product.description} Lorem ipsum dolor sit amet,
+                        consectetur adipisicing elit. Quisquam, est labore
+                        repudiandae ullam dignissimos veritatis unde quos
+                        officia sit facilis vel rerum laudantium iusto culpa!
+                        Placeat nisi laboriosam quasi non.
+                    </p>
+                </div>
+                <div className={styles['specs-container']}>
+                    <Specifications properties={product.properties} />
                 </div>
             </div>
-            <div className={styles['description']}>
-                <h2 className={styles['description__heading']}>
-                    Product Description
-                </h2>
-                <p className={styles['description__text']}>
-                    {product.description} Lorem ipsum dolor sit amet,
-                    consectetur adipisicing elit. Quisquam, est labore
-                    repudiandae ullam dignissimos veritatis unde quos officia
-                    sit facilis vel rerum laudantium iusto culpa! Placeat nisi
-                    laboriosam quasi non.
-                </p>
-            </div>
-            <div className={styles['specs-container']}>
-                <Specifications properties={product.properties} />
-            </div>
-        </div>
+
+            <Modal
+                sx={{ backdropFilter: 'blur(10px)' }}
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <div className={styles['modal-heading']}>Enquiry Form</div>
+                    <form
+                        className={styles['form']}
+                        onSubmit={formSubmitHandler}
+                    >
+                        <div className={styles['form__group']}>
+                            <label htmlFor="">
+                                Name{' '}
+                                <span className={styles['red-star']}>*</span>
+                            </label>
+                            <input
+                                placeholder="Enter Name"
+                                ref={nameRef}
+                                required
+                                type="text"
+                            />
+                        </div>
+                        <div className={styles['form__group']}>
+                            <label htmlFor="">
+                                Country{' '}
+                                <span className={styles['red-star']}>*</span>
+                            </label>
+                            <input
+                                placeholder="Enter Country eg. India"
+                                ref={countryRef}
+                                required
+                                type="text"
+                            />
+                        </div>
+                        <div className={styles['form__group']}>
+                            <label htmlFor="">
+                                Phone Number{' '}
+                                <span className={styles['red-star']}>*</span>
+                            </label>
+                            <input
+                                placeholder="Enter Phone"
+                                ref={phoneRef}
+                                required
+                                type="text"
+                            />
+                        </div>
+                        <div className={styles['form__group']}>
+                            <label htmlFor="">Email Address</label>
+                            <input
+                                placeholder="Enter email"
+                                name={emailRef}
+                                type="email"
+                            />
+                        </div>
+                        <div className={styles['form__group']}>
+                            <label ref={descriptionRef} htmlFor="">
+                                Description
+                            </label>
+
+                            <textarea name="" id=""></textarea>
+                        </div>
+                        <div className={styles['form__group--submit']}>
+                            <button
+                                type="submit"
+                                className={styles['submit-btn']}
+                            >
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+                </Box>
+            </Modal>
+        </>
     );
 };
 
@@ -57,4 +156,18 @@ export const productLoader = async ({ request }) => {
     const product = resp.data.data.product;
 
     return product;
+};
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    // width: 400,
+    bgcolor: '#eeefff',
+
+    // border: '2px solid #000',
+    borderRadius: 5,
+    boxShadow: 24,
+    p: 5,
 };
